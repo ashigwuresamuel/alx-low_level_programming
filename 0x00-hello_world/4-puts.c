35d6b1298a0ce12a3288b432d6b7a48eec4fa6a4
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-15 13:12:46 -0700

    Prints words with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
- #include <stdio.h>
- /**
- * main - main block
- * return:0
- * */
- int main(void)
- {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
-} 
+	#include <stdio.h>
+/**
+*main - is the starting point of the program
+*the main function return:0
+*return(0):return the value 0 during compilation
+*/
+	int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+1  #include <stdio.h>
+2    /**
+3    * main - main block
+4    * Return: 0
+5    */
+6  int main(void)
+7   {
+8  print("with proper grammar, but the outcome is a piece of art,");
+9  return (0);
+10  }
