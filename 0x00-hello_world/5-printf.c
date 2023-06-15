d549aa82bd61442962b16f1cccbd87827ca4b890
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-15 15:43:50 -0700

    Shell Scripts that prints strings followed by a new line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-1  #include <stdio.h>
-2    /**
-3    * main - main block
-4    * Return: 0
-5    */
-6  int main(void)
-7   {
-8  print("with proper grammar, but the outcome is a piece of art,");
-9  return (0);
-10  }
+	#include <stdio.h>
+/**
+* main - main block_start point of our code
+* Return: 0 (Success)
+*/
+	int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
