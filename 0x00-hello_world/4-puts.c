ab94d521fac4587659c522df6c7f9b90e31730e8
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-15 15:29:21 -0700

    Scripts that prints with the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-	#include <stdio.h>
+#include <stdio.h>
 /**
-*main - is the starting point of the program
-*the main function return:0
+*main - a program that prints a line with puts function
 *Return: Always 0 (success)
 */
-	int main(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
