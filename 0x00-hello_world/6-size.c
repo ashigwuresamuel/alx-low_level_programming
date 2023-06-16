32b207cde4556c243eed353f63dd63686a3f0d67
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-15 18:13:53 -0700

    C program that prints size of types on the computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+	#include <stdio.h>
+/**
+* main - main block startin_poingt of our code
+* A program to find the of char,int long int,long long int and float
+* Return: 0 always (Success)
+*/
+
+	int main(void)
+	(
+	printf("Size of char: %c byte(S\n)", sizof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lli byte(s)\n", sizeof(long long int));
+	prinf("Size of a float: %i bytes\n", sizeof(float));
+	return (0);
+	}
