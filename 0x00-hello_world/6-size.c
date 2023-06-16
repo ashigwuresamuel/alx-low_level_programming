35ef9177cfabb355ead58bc4f3d279182a131e5a
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-15 18:48:17 -0700

    Script that generates the assembly code (Intel syntax) of a C code and save it in an output file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,17 @@
-	#include <stdio.h>
+#include <stdio.h>
 /**
 * main - main block startin_poingt of our code
 * A program to find the of char,int long int,long long int and float
 * Return: 0 always (Success)
 */
 
-	int main(void)
-	(
-	printf("Size of char: %i byte(S)\n", sizof(char));
-	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-	prinf("Size of a float: %i byte(s)\n", sizeof(float));
-	return (0);
-	}
+int main(void)
+(
+
+printf("Size of char: %lu byte(S)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(b));
+printf("Size of a long int: %lu byte(s)\n",(unsigned long) sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+prinf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
