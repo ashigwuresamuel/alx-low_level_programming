6976a4f0fa220c0cc7d2f7da710450905f4e22e6
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-15 19:13:31 -0700

    C program that prints strings and characters with new line to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main block, starting point of our code
+ * Return: 1
+ */
+int mian(void)	
+(
+putc("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+)
