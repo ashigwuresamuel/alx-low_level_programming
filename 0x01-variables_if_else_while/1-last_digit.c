ca92da94d46c303be86cdada68a4971de7ac5cdb
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-16 16:44:36 -0700

    prints last digit of randomly generated  numberr

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	else if ((n % 10) < 6 && (n % 10) != 0)
 
 	{
-		print("Last digit of %d is %d and is less than 8 and not 0\m", n, n % 10);
+		printf("Last digit of %d is %d and is less than 8 and not 0\n", n, n % 10);
 	}
 
 	else
