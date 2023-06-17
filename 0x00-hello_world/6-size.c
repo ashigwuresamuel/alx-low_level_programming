aa19635d885baba84cdc12105c1ce2af95229a83
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-17 16:49:09 -0700

    program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 /**
 * main - Entry point
 * A program to find the of char,int long int,long long int and float
-* Return: 0 always (Success)
+* Return: 0 always (success)
 */
-
 int main(void)
 {
-printf("Size of char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
-return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
 }
