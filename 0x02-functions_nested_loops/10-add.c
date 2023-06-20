afbbe2f1264f3a13d56aa6b89afc678b14e65a1a
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-20 14:34:56 -0700

    Functions and Nested Loops tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @e: int to be added to b
+ * @f: int to be added to a
+ * Return: sum of e and f
+ */
+int add(int e, int f)
+{
+	int g;
+
+	g = e + f;
+
+	return (g);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ALX full-stack program, functoins- function declaration,referencing of function, function calls and Nested loop_ a loop inside a previous loop.
