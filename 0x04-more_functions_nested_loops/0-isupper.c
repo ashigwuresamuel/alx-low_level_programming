085a343d616537d62fa39f419ea6822754791bd8
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-06-22 14:15:25 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - checks if c is uppercase Returns 1 if c is uppercase
+* Returns 0 otherwise
+*@c: character to be checked.
+* Return:1 or 0.
+*/
+int _isupper(int c)
+{
+
+if (c >= 'A' &&  c <= 'Z')
+return (1);
+
+else
+
+return (0);
+}
