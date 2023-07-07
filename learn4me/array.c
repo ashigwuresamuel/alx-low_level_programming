c05250ceb47b9ca160afb1676130c3a6aba68815
EMMANUEL SAMUEL ASHIGWURE <emmanuelsamuelashigwure@gmail.com>
2023-07-07 04:24:43 -0700

    C - 0x08-recursion

---
+++ learn4me/array.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+/**
+* main - Prints arrays
+* Return: 0 A Always(Successful)
+*/
+
+int main(void)
+{
+
+int disp[2][3];
+
+int i, j;
+
+for (i = 0; i < 2; i++)
+{
+
+for (j = 0; j < 3; j++)
+
+{
+
+printf("Enter value for disp[%d][%d]:", i, j);
+
+scanf("%d", &disp[i][j]);
+
+}
+
+}
+
+/*Displaying array elements*/
+printf("Two Dimensional array elements:\n");
+
+for (i = 0; i < 2; i++)
+{
+for (j = 0; j < 3; j++)
+{
+printf("%d ", disp[i][j]);
+if (j == 2)
+{
+printf("\n");
+}
+}
+}
+return (0);
+}
